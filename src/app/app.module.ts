dcbe6589670cdaafdfd9ba87e22f48f9bbb708e5
jignesh-parekh-roche <jigneshkumar.parekh@roche.com>
2017-07-10 15:08:24 -0700

    Moved hero-detail component under 'HeroDetails' folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
 
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroDetailComponent } from './HeroDetail/hero-detail.component';
 
 import { AppComponent }  from './app.component';
 
